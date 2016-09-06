b7db5ddaebdf7018c49870920d0cc0b1f7ba4f2b
mykedi <nastya.kedi@mail.ru>
2016-09-06 23:00:36 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
+var link = document.querySelector(".research_hotels");
+var form = document.querySelector(".research");
 
-    var link = document.querySelector(".research_hotels");
-    var form = document.querySelector(".research");
-            
+link.addEventListener("click", function(event) {
+event.preventDefault();
+form.classList.toggle("research_show");
+});
 
-    link.addEventListener("click", function(event) {
-    event.preventDefault();
-    form.style.display = (form.style.display == "block") ? "none" : "block"
-   }); 
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+	if (form.classList.contains("research_show")) {
+	form.classList.remove("research_show");
+	}
+	}
+});
 
-    window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-            form.style.display = "none";
-          }
-    });
